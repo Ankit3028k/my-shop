dff5c013aec07e5ed0cc96895219d035dfe90778
Ankit Gangrade <158150573+Ankit3028k@users.noreply.github.com>
2024-10-24 12:21:38 +0530

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://ankit3028k.github.io/personal-shop-backend//api/v1', // Update with your API URL
+    baseURL: 'https://ankit3028k.github.io/personal-shop-backend/api/v1', // Update with your API URL
 });
 
 export default instance;
