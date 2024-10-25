b0b30ac29760829b824b4f65bcb2258c386d2718
Ankit Gangrade <158150573+Ankit3028k@users.noreply.github.com>
2024-10-25 16:39:04 +0530

    Update Products.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -9,7 +9,7 @@ const Products = () => {
         <div className=" bg-white min-h-screen">
             <Navbar /> 
 
-            {/* <h1 className="text-4xl font-semibold text-center text-gray-800 mb-6">Products</h1> */}
+             <h1 className="text-4xl font-semibold text-center text-gray-800 mb-6">Products</h1> 
             <ProductList />
             <Footer/>
         </div>
