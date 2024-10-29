2c09ac838805bf70faf50f2eebef63e4cd755d21
Ankit Gangrade <158150573+Ankit3028k@users.noreply.github.com>
2024-10-29 16:11:25 +0530

    Update CategoryList.js

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -19,7 +19,7 @@ const CategoryList = ({ setSelectedCategory }) => {
 
     return (
         <div className="">
-            <h2 className="text-2xl font-bold mb-4">Categories</h2>
+            <h2 className="text-2xl font-bold ">Categories</h2>
             <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
                 {categories.map(category => (
                     <li 
