ad3c74d4dd6ea67dc1f0079241bd6a17de156fe4
Ankit Gangrade <158150573+Ankit3028k@users.noreply.github.com>
2024-11-07 17:57:13 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_URL = 'https://personal-shop-backend.vercel.app/api/v1'; // Update based on your backend URL
+const API_URL = 'https://personal-shop-backend.onrender.com/api/v1'; // Update based on your backend URL
 
 export const fetchCategories = () => {
     return axios.get(`${API_URL}/categories`);
